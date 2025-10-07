285675acaba077ec01f1bd571f8f07fb74c26a10
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2025-10-07 18:13:11 +0400

    feat: user access token refresher and usr by refresh token routes

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -3,8 +3,11 @@ import { Request, Response } from "express";
 import { validationResult } from "express-validator";
 import {
   RegisterUserService,
+  UserByTokenService,
   UserLoginService,
+  UserTokenRefreshService,
 } from "../services/user.service";
+import sequelize from "sequelize";
 
 export async function UserRegisterController(req: Request, res: Response) {
   try {
@@ -39,8 +42,20 @@ export async function UserLoginController(req: Request, res: Response) {
 
     const result = await UserLoginService(data);
 
-    // If your service throws an error, this line won’t run
-    // So we can safely assume result exists and is valid
+    res.cookie("accessToken", result.accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 15 * 60 * 1000, // 15 min
+    });
+
+    res.cookie("refreshToken", result.refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+
     return res.status(200).json({
       message: result.message,
       accessToken: result.accessToken,
@@ -50,6 +65,53 @@ export async function UserLoginController(req: Request, res: Response) {
     console.error("Login controller error:", error);
     return res.status(500).json({
       message: "Something went wrong while logging in",
+      error: error.message,
+    });
+  }
+}
+
+export async function UserTokenRefreshController(req: Request, res: Response) {
+  try {
+    const cookie = req.headers.cookie;
+
+    const cookies = cookie
+      ?.split("; ")
+      .reduce<Record<string, string>>((acc, item) => {
+        const [key, value] = item.split("=");
+        acc[key] = value;
+        return acc;
+      }, {});
+
+    const accessToken = cookies?.["accessToken"];
+    const refreshToken = cookies?.["refreshToken"];
+
+    const newAcceesToken = await UserTokenRefreshService(
+      accessToken,
+      refreshToken
+    );
+
+    return res.status(200).json({
+      accessToken: newAcceesToken,
+    });
+  } catch (error: any) {
+    console.log(error);
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+}
+
+export async function UserByTokenController(req: Request, res: Response) {
+  try {
+    const headers = req.headers["authorization"];
+    const token = headers?.split(" ")[1];
+
+    const user = await UserByTokenService(token);
+
+    return res.status(200).json(user);
+  } catch (error: any) {
+    console.log(error);
+    return res.status(500).json({
       error: error.message,
     });
   }
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import {
+  UserByTokenController,
   UserLoginController,
   UserRegisterController,
+  UserTokenRefreshController,
 } from "../controller/user.controller";
 const router = express.Router();
 
 router.post("/register", UserRegisterController);
 router.post("/login", UserLoginController);
+router.get("/me", UserByTokenController);
+router.get("/refresh", UserTokenRefreshController);
 
 export default router;
