a6458a7085d7de2d2a07d9021e2052c2f954a99c
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2025-10-09 14:52:14 +0400

    feat: featured products route

--- src/controller/products.controller.ts
+++ src/controller/products.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import {
   AllProductsService,
+  FeaturedProductsService,
   VipProductsService,
 } from "../services/product.service";
 
@@ -25,6 +26,18 @@ export async function VipProductsController(req: Request, res: Response) {
     return res.status(200).json(vipProducts);
   } catch (error: any) {
     console.log(error);
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+}
+
+export async function FeaturedProductsController(req: Request, res: Response) {
+  try {
+    const feturedProducts = await FeaturedProductsService();
+
+    return res.status(200).json(feturedProducts);
+  } catch (error: any) {
     return res.status(500).json({
       error: error.message,
     });
--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import {
   AllProductsController,
+  FeaturedProductsController,
   VipProductsController,
 } from "../controller/products.controller";
 const router = express.Router();
 
 router.get("/", AllProductsController);
 router.get("/vip", VipProductsController);
+router.get("/featured", FeaturedProductsController);
 
 export default router;
