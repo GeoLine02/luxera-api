d03c04ad70164ffa3e5638575a3d276da4c3f808
kaikaci12 <somewonesomewone@gmail.com>
2025-11-22 09:35:21 -0800

    revert sequelize migrations

--- src/sequelize/migrations/20251030094957-create-product-variants.js
+++ src/sequelize/migrations/20251030094957-create-product-variants.js
@@ -9,7 +9,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-      product_variant: {
+      variant_name: {
         type: Sequelize.STRING,
         allowNull: false,
       },
--- src/sequelize/migrations/20251102121383-add-new-properties-to-product-variants.js
+++ src/sequelize/migrations/20251102121383-add-new-properties-to-product-variants.js
@@ -4,24 +4,29 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     // Add new columns to ProductVariants
-    await queryInterface.addColumn("ProductVariants", "variantPrice", {
+    await queryInterface.addColumn("ProductVariants", "variant_price", {
       type: Sequelize.DECIMAL,
       allowNull: false,
       defaultValue: 0,
     });
-
-    await queryInterface.renameColumn(
-      "ProductVariants",
-      "product_variant",
-      "variant_name"
-    );
+    
+    await queryInterface.addColumn("ProductVariants", "variant_quantity", {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+    });
+    await queryInterface.addColumn("ProductVariants", "variant_discount", {
+      type: Sequelize.DECIMAL,
+      allowNull: false,
+      defaultValue: 0,
+    });
   },
 
   async down(queryInterface, Sequelize) {
 
-    await queryInterface.removeColumn("ProductVariants", "variantPrice");
-
-
+    await queryInterface.removeColumn("ProductVariants", "variant_price");
+    await queryInterface.removeColumn("ProductVariants", "variant_quantity");
+    await queryInterface.removeColumn("ProductVariants", "variant_discount");
     await queryInterface.renameColumn(
       "ProductVariants",
       "variant_name",
