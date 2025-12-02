5412958327db44e33c9db9d765d798235c56a926
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2025-12-02 16:35:19 +0400

    fix: remove addColumn command from sequelize migration

--- src/sequelize/migrations/20251201195616-products-add-reject-status-and-remove-product-name.js
+++ src/sequelize/migrations/20251201195616-products-add-reject-status-and-remove-product-name.js
@@ -3,8 +3,6 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.removeColumn("Products", "product_name");
-
     await queryInterface.changeColumn("Products", "product_status", {
       type: Sequelize.ENUM("pending", "active", "vip", "rejected"),
       allowNull: false,
@@ -13,11 +11,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.addColumn("Products", "product_name", {
-      type: Sequelize.STRING,
-      allowNull: false,
-    });
-
     await queryInterface.changeColumn("Products", "product_status", {
       type: Sequelize.ENUM("pending", "active", "vip"),
       allowNull: false,
