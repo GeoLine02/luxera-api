e942dc5b2a5a2b20bee8d11f97b6afe4c84e93ee
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2025-12-27 23:12:56 +0400

    remove allowNull prop form product variants image remover migration

--- src/sequelize/migrations/20251130131530-remove-image-from-product-variants.js
+++ src/sequelize/migrations/20251130131530-remove-image-from-product-variants.js
@@ -9,7 +9,6 @@ module.exports = {
   async down(queryInterface, Sequelize) {
     await queryInterface.addColumn("ProductVariants", "image", {
       type: Sequelize.STRING,
-      allowNull: false,
     });
   },
 };
