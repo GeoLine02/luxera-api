edeea2e12516105b0e1eea8e1e864b891cdd25a0
Tato <somewonesomewone@gmail.com>
2026-01-29 17:23:14 +0400

    fix transport conf

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -341,9 +341,7 @@ export const getOTPEmailTemplate = (otp: string, userName?: string): string => {
 };
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: 587,
-  secure: false, // Use true for port 465, false for port 587
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
